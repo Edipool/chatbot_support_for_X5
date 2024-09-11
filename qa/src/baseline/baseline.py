d7e34f46de3aebf26743ddb206aaa7e5b44b5ab9
Артем Волков <artemwulf2015@gmail.com>
2024-09-11 20:29:47 +0300

    Integrate model to fastapi

--- qa/src/baseline/baseline.py
+++ qa/src/baseline/baseline.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from src.baseline.model import ChatbotModel
+
+chatbot = None
 
 app = FastAPI()
 
 @app.get("/query")
 async def handle_query(query: str):
-    if query == "Как твои дела":
-        return {"answer": "нормально"}
-    else:
-        return {"answer": "turn on to human"}
+    global chatbot
+    return chatbot._predict_with_tfidf(query)
 
 if __name__ == "__main__":
+    df = pd.read_excel('../../data/LK_modified.xlsx', sheet_name=1)
+    category_counts = df['content'].value_counts()
+    small_categories = category_counts[category_counts < 7].index
+    df['content'] = df['content'].replace(small_categories, 'поддержка')
+    label_encoder = LabelEncoder().fit(df['content'])
+    chatbot = ChatbotModel(df, label_encoder)
     uvicorn.run(app, host="0.0.0.0", port=8081)
